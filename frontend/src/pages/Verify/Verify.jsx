e93bc1ab4a49216bfa410eb20dbb3248fa5a78e7
srikar <srikarvanaparthy@gmail.com>
2025-01-21 17:06:23 +0530

    added files

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -15,7 +15,7 @@ const Verify = () => {
     const verifyPayment = async ()=>{
         const response = await axios.post(url+"/api/order/verify",{success,orderId})
         if(response.data.success){
-            navigate('/myorders');
+            navigate('/myorders')
         }else{
             navigate('/')
         }
@@ -28,7 +28,7 @@ const Verify = () => {
   return (
     <div className='verify'>
         <div className="spinner">
-            
+
         </div>
 
     </div>
